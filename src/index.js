76eae3112e1292729fef84951ea53efe6cb79d19
Murat <cryptmensis@gmail.com>
2023-01-27 19:09:18 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -35,11 +35,40 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '' : ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+  arr = splitString(arr.join(''), 2)
+  
+  for (variable in arr){
+    if (arr[variable] === '10') {
+    arr[variable] = '.'
+    }
+    else if (arr[variable] === '11') {arr[variable] = '-'}
+    else arr[variable] = ' '
+  }
+  arr = splitString(arr.join(''), 5)
+  for (variable in arr){
+    arr[variable] = arr[variable].trim()
+  }
+  let res = ''
+  for (variable in arr){
+    res += MORSE_TABLE[arr[variable]]
+  }
+
+    return res
 }
+function splitString(str, N) {
+    const arr = [];
+  
+    for (let i = 0; i < str.length; i += N) {
+      arr.push(str.substring(i, i + N));
+    }
+  
+    return arr;
+  }
 
 module.exports = {
     decode
